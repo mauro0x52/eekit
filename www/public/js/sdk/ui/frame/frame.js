1893df3b0d858de1b39f37e6503fb69027727808
Mauro Ribeiro <mauro0x52@gmail.com>
2013-02-19 17:08:01 -0300

    #1 Atualizações para os framesno www e home para visitante deslogado

--- www/public/js/sdk/ui/frame/frame.js
+++ www/public/js/sdk/ui/frame/frame.js
@@ -40,22 +40,27 @@ sdk.modules.ui.frame = function (app) {
 
     /* Métodos públicos */
     this.height = function (value) {
-        var height = 460;
+        var height = 460,
+            width;
 
         if (document.body && document.body.offsetWidth) {
             height = document.body.offsetHeight;
+            width  = document.body.offsetWidth;
         }
         if (document.compatMode=='CSS1Compat' &&
             document.documentElement &&
             document.documentElement.offsetWidth
         ) {
             height = document.documentElement.offsetHeight;
+            width  = document.documentElement.offsetWidth;
         }
         if (window.innerWidth && window.innerHeight) {
             height = window.innerHeight;
+            width  = window.innerWidth;
         }
 
-        body_div.style.height = height-155;
+        body_div.style.height = height-120;
+        body_div.style.width = width;
     };
     this.loading = function (value) {
         if (value) {
