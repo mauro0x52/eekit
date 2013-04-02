4e8966536812784e7d3c88fd19db79a395a7a514
Lamasu <lamasu@kamino.(none)>
2013-04-02 15:09:56 -0300

    FUnções utilitárias para o tracker eo kamisama para pegar os tokens

--- kami-sama/Utils.js
+++ kami-sama/Utils.js
@@ -7,26 +7,40 @@
  */
 var config = require('./config.js');
 
-/** Auth
+/** Tokens
  * @author : Rafael Erthal
- * @since : 2012-07
+ * @since : 2013-04
  *
- * @description : valida o token de um usuário no serviço Auth
- * @param login : username do usuário
+ * @description : retorna os tokens de um usuário
  * @param token : token do usuário
- * @param cb : callback a ser chamado após validado o token
+ * @param cb : callback a ser chamado
  */
-exports.auth = function (token, cb) {
+exports.tokens = function (token, cb) {
     "use strict";
-    
-    require('restler').get('http://'+config.services.auth.url+':'+config.services.auth.port+'/validate', {
+
+    require('restler').get('http://'+config.services.auth.url+':'+config.services.auth.port+'/service/kamisama/authorize', {
         data: {
             token  : token,
             secret : config.security.secret
         }
     }).on('success', function(data) {
-        if (data.company) {
-            cb(null, data.company);
+        if (data.token) {
+            require('restler').get('http://'+config.services.auth.url+':'+config.services.auth.port+'/validate', {
+                data: {
+                    token  : data.token,
+                    secret : config.security.secret
+                }
+            }).on('success', function(data) {
+                if (data.tokens) {
+                    cb(null, data);
+                } else if (data.error) {
+                    cb(data.error, null);
+                } else {
+                    cb(null, null);
+                }
+            }).on('error', function(error) {
+                cb(error, null);
+            });
         } else if (data.error) {
             cb(data.error, null);
         } else {
@@ -34,55 +48,30 @@ exports.auth = function (token, cb) {
         }
     }).on('error', function(error) {
         cb(error, null);
-    });
+    });    
 };
 
-/** Bind
+/** Services
  * @author : Rafael Erthal
- * @since : 2013-03
+ * @since : 2013-04
  *
- * @description : binda evento no kami-sama
- * @param name : nome do evento
- * @param method : método para chamar a url
- * @param callback : url a ser chamada no disparo do evento
+ * @description : lista serviços registrados
+ * @param cb : callback a ser chamado
  */
-exports.bind = function (token, name, method, callback) {
+exports.services = function (cb) {
     "use strict";
     
-    require('restler').post('http://' + config.services.kamisama.url + ':' + config.services.kamisama.port + '/bind', {
-        data: {
-            token : token,
-            secret : config.security.secret,
-            label : name,
-            method : method,
-            callback : callback
+    require('restler').get('http://'+config.services.auth.url+':'+config.services.auth.port+'/services').on('success',
+        function(data) {
+            if (data.services) {
+                cb(null, data.services);
+            } else if (data.error) {
+                cb(data.error, null);
+            } else {
+                cb(null, null);
+            }
         }
-    }).on('success', function() {}).on('error', function() {});
-};
-
-/** Trigger
- * @author : Rafael Erthal
- * @since : 2013-03
- *
- * @description : dispara evento no kami-sama
- * @param name : nome do evento
- * @param method : método para chamar a url
- * @param callback : url a ser chamada no disparo do evento
- */
-exports.trigger = function (token, name, data) {
-    "use strict";
-
-    var post = {
-        token : token,
-        secret : config.security.secret,
-        label : name
-    }
-
-    for (var prop in data) {
-        post['data[' + prop + ']'] = data[prop]
-    }
-
-    require('restler').post('http://' + config.services.kamisama.url + ':' + config.services.kamisama.port + '/trigger', {
-        data: post
-    }).on('success', function() {}).on('error', function() {});
+    ).on('error', function(error) {
+        cb(error, null);
+    });
 };
