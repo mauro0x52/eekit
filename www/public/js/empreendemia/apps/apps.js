ed261988866f5b642c7c59b8fd1b686d53b95d60
Lamasu <lamasu@kamino.(none)>
2013-04-04 08:35:32 -0300

    acertando filtro do tasks

--- www/public/js/empreendemia/apps/apps.js
+++ www/public/js/empreendemia/apps/apps.js
@@ -24,48 +24,50 @@ empreendemia.apps = {
                 }
             }
 
-            empreendemia.ajax.get({
-                url : 'http://' + empreendemia.config.services.apps.host + ':' + empreendemia.config.services.apps.port + '/app/' + params.app + '/source'
-            }, function (response) {
-                if (response && !response.error) {
-                    var tool = sdk.open(response);
+            empreendemia.user.companyUsers(function () {
+                empreendemia.ajax.get({
+                    url : 'http://' + empreendemia.config.services.apps.host + ':' + empreendemia.config.services.apps.port + '/app/' + params.app + '/source'
+                }, function (response) {
+                    if (response && !response.error) {
+                        var tool = sdk.open(response);
 
-                    tool.close = function (data) {
-                        var i,
-                            found = false,
-                            nav = empreendemia.ui.content.navigation.navigables.get(),
-                            apps = empreendemia.ui.content.roll.sheets.apps.get();
+                        tool.close = function (data) {
+                            var i,
+                                found = false,
+                                nav = empreendemia.ui.content.navigation.navigables.get(),
+                                apps = empreendemia.ui.content.roll.sheets.apps.get();
 
-                        tool.ui.detach();
+                            tool.ui.detach();
 
-                        for (i in apps) {
-                            if (apps[i] === tool.ui) {
-                                found = true;
-                                empreendemia.ui.content.navigation.focus(apps[i-1]);
+                            for (i in apps) {
+                                if (apps[i] === tool.ui) {
+                                    found = true;
+                                    empreendemia.ui.content.navigation.focus(apps[i-1]);
+                                }
+                                if (found) {
+                                   empreendemia.ui.content.navigation.navigables.remove(nav[i]);
+                                   empreendemia.ui.content.roll.sheets.apps.remove(apps[i]);
+                               }
                             }
-                            if (found) {
-                               empreendemia.ui.content.navigation.navigables.remove(nav[i]);
-                               empreendemia.ui.content.roll.sheets.apps.remove(apps[i]);
-                           }
+
+                            if (params.close) {
+                                params.close(data);
+                            }
+                        };
+
+                        tool.open = function (data) {
+                            tool.routes.go(params.route, data);
+                        };
+
+                        tool.caller = function () {
+                            return params.caller;
+                        };
+
+                        if (params.open) {
+                            params.open(tool);
                         }
-
-                        if (params.close) {
-                            params.close(data);
-                        }
-                    };
-
-                    tool.open = function (data) {
-                        tool.routes.go(params.route, data);
-                    };
-
-                    tool.caller = function () {
-                        return params.caller;
-                    };
-
-                    if (params.open) {
-                        params.open(tool);
                     }
-                }
+                });
             });
         }
     },
