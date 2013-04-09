ea06e3ba10b7d4031d3fdd443c5f69663a67b465
Mauro Ribeiro <mauro0x52@gmail.com>
2013-04-09 09:05:08 -0300

    Corrigindo categorias do sendgrid

--- auth/controller/Company.js
+++ auth/controller/Company.js
@@ -90,7 +90,7 @@ module.exports = function (app) {
                                             token : token,
                                             service : 'profiles',
                                             subject : 'Presente de boas vindas do Empreendekit',
-                                            name : 'novo usuário',
+                                            name : 'novo usuario',
                                             from : 'lucas@empreendemia.com.br',
                                             html : '<p>Olá '+userData.name + ', tudo bom?</p><p>Você se cadastrou no EmpreendeKit e acabou de dar o primeiro passo para tornar sua empresa mais produtiva.</p><p>Para te ajudar nessa jornada, estou te enviando um ebook que acabamos de lançar: "Produtividade sem Enrolação".</p><p>Para baixar gratuitamente o ebook, clique <a href="http://pages.rdstation.com.br/livro-produtividade?utm_source=Etapa2&utm_medium=Email-semana2&utm_content=Contatos-poscadastro&utm_campaign=LC02">aqui</a>.</p><p>Qualquer dúvida sobre o EmpreendeKit, pode mandar um email direto para mim.</p><p>Abraços,<br />Lucas</p><br /><br />'
                                         }
@@ -100,7 +100,7 @@ module.exports = function (app) {
                                         data : {
                                             token : token,
                                             service : 'profiles',
-                                            subject : 'Novo usuário cadastrado',
+                                            subject : 'Novo usuario cadastrado',
                                             name : 'novo usuário',
                                             to : 'lucas@empreendemia.com.br',
                                             html : '<p>Nome: '+ userData.name + '</p><p>Telefone: ' + (userData.informations && userData.informations.phone ? userData.informations.phone : '') + '</p>'
