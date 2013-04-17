ee7857279cb02301bb7077786a480705fd96f538
Lamasu <lamasu@kamino.(none)>
2013-04-17 08:32:53 -0300

    #157 resolvido, só falta mauro fazer o visual da botão fechar

--- www/public/js/empreendemia/ui/elements/helper.js
+++ www/public/js/empreendemia/ui/elements/helper.js
@@ -8,6 +8,9 @@
 
 empreendemia.ui.helper = function (parent) {
     var element = document.createElement('div'),
+        close_div = document.createElement('div'),
+        close_image_div = document.createElement('div'),
+        close_legend_div = document.createElement('div'),
         description_div = document.createElement('div'),
         example_div = document.createElement('div'),
         arrow_div = document.createElement('div'),
@@ -16,6 +19,10 @@ empreendemia.ui.helper = function (parent) {
 
     /* CSS */
     element.setAttribute('class', 'helper hide');
+    close_div.setAttribute('class', 'close');
+    close_image_div.setAttribute('class', 'image');
+    close_legend_div.setAttribute('class', 'legend');
+    close_legend_div.innerHTML = 'fechar';
     description_div.setAttribute('class', 'description');
     example_div.setAttribute('class', 'example');
     arrow_div.setAttribute('class', 'arrow');
@@ -23,12 +30,20 @@ empreendemia.ui.helper = function (parent) {
 
     /* Hierarquia */
     parent.appendChild(element);
+    element.appendChild(close_div);
+    close_div.appendChild(close_image_div);
+    close_div.appendChild(close_legend_div);
     element.appendChild(description_div);
     element.appendChild(example_div);
     element.appendChild(arrow_div);
     arrow_div.appendChild(fill_div);
 
     /* Eventos */
+    close_div.addEventListener('click', function (event) {
+        event.stopPropagation();
+        that.hide();
+    }, true);
+    
     if (parent && parent.addEventListener) {
         parent.addEventListener('click', function () {
             that.hide();
