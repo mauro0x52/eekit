1ad2486c9b516f456790421840a8a28283439a99
Mauro Ribeiro <mauro0x52@gmail.com>
2013-05-02 14:57:38 -0300

    Colocando senha no monitor

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -4,16 +4,28 @@ var express = require('express'),
 var app = express();
 
 app.get('/service/:id/restart', function (request, response) {
+    if (request.param('secret', null) != 'm0n1t0rm4ne1r0p4r4vcs3rf3l1z') {
+        response.end();
+        return;
+    }
     forever.restart(request.params.id + '.js');
     response.end('services restarted');
 });
 
 app.get('/services/restart', function (request, response) {
+    if (request.param('secret', null) != 'm0n1t0rm4ne1r0p4r4vcs3rf3l1z') {
+        response.end();
+        return;
+    }
     forever.restartAll()
     response.end('services restarted');
 });
 
 app.get('/resources', function (request, response) {
+    if (request.param('secret', null) != 'm0n1t0rm4ne1r0p4r4vcs3rf3l1z') {
+        response.end();
+        return;
+    }
     var exec = require('child_process').exec;
     exec('uptime', function (error, stdout, stderr) {
         response.send(stdout)
@@ -22,6 +34,10 @@ app.get('/resources', function (request, response) {
 });
 
 app.get('/service/:id/log', function (request, response) {
+    if (request.param('secret', null) != 'm0n1t0rm4ne1r0p4r4vcs3rf3l1z') {
+        response.end();
+        return;
+    }
     forever.tail(request.params.id + '.js', function (error, services) {
         var log = services[0].logs;
         for (var i in log) {
