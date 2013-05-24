c8d6830b98de9c4dba3067addc7fdd9600636d86
lamasu <rafaerthal@gmail.com>
2013-05-24 15:28:05 -0300

    inserindo kami-sama

--- www/public/js/auth/auth.js
+++ www/public/js/auth/auth.js
@@ -14,6 +14,12 @@ module.exports({
                 setToken(data.token, data.remindme);
                 Empreendekit.config.services.www.token = data.token;
                 Empreendekit.path.redirect('tarefas/');
+
+                empreendemia.socket.emit('auth', {
+                    user    : empreendemia.config.user,
+                    company : empreendemia.config.company
+                });
+
             }});
         },
 
@@ -22,7 +28,16 @@ module.exports({
         },
 
         signout : function () {
+            setToken(null, 0);
+        },
 
+        validate : function (cb) {
+            Empreendekit.ajax.get({url : 'http://' + Empreendekit.config.services.auth.host + ':' + Empreendekit.config.services.auth.port + '/validate', data : {
+                secret : Empreendekit.config.services.www.secret,
+                token : getToken()
+            }}, function (data) {
+                cb(data);
+            });
         }
 
     },
